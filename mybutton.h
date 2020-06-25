9d8553231e5150396d27eb6b662e57266f25a94b
Your Name <you@example.com>
2020-06-25 22:58:18 +0800

    重启大作业---开始界面转到map的实现

---
+++ mybutton.h
@@ -0,0 +1,16 @@
+#ifndef MYBUTTON_H
+#define MYBUTTON_H
+
+#include <QWidget>
+
+class mybutton : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit mybutton(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // MYBUTTON_H
